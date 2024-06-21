a3dd239859cc01ff68cd736bc5e85965e55be62f
uri <katyacitti@gmail.com>
2024-06-21 18:01:43 +0300

    project created

---
+++ src/test/java/ru/netology/DeliveryCardTest.java
@@ -0,0 +1,34 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class DeliveryCardTest {
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void verification() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Санкт-Петербург");
+        String date = generateDate(5, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(date);
+        $("[data-test-id='name'] input").setValue("Денисова Екатерина");
+        $("[data-test-id='phone'] input").setValue("+79270000000");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + date));
+    }
+}
